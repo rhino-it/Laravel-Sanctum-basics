61940346c5dd1a872855bac5dcc11fa5cb9e5c1e
rhino-it <it.academy.osh@gmail.com>
2021-11-17 18:04:03 +0600

    first commit

---
+++ routes/api.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+// Public routes
+// REST API STARTS
+Route::post('/register',[AuthController::class, 'register']);
+Route::post('/login',[AuthController::class, 'login']);
+Route::get('/products',[ProductController::class, 'index']);
+Route::get('/products/{product}',[ProductController::class, 'show']);
+
+// Route::get('/products/search/{name}',[ProductController::class, 'search']);
+// REST API ENDS
+
+// Protected routes
+Route::group(['middleware'=>['auth:sanctum']], function () {
+    Route::post('/products',[ProductController::class, 'store']);
+    Route::put('/products/{product}',[ProductController::class, 'update']);
+    Route::delete('/products/{product}',[ProductController::class, 'destroy']);
+    Route::post('/logout',[AuthController::class, 'logout']);
+});
